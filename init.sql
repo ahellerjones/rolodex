81da003397072ac044b313889df3eb9263a6afa4
garcgabe <garciagabe0033@yahoo.com>
2023-06-01 22:28:57 -0400

    base queries done

--- init.sql
+++ init.sql
@@ -2,21 +2,21 @@
 -- Dim Users table
 
 CREATE TABLE users(
-    userID int AUTO_INCREMENT primary key,
+    userID AUTO_INCREMENT primary key,
     login_name varchar(64) unique not null,
     password varchar(256) not null
     )
 -- Mod Contacts table
 
 CREATE TABLE contacts(
-    contactID int AUTO_INCREMENT primary key,
+    contactID AUTO_INCREMENT primary key,
     userID int, 
     name varchar(256),
     address varchar(256),
-    phonenumber varchar(64), 
+    phone_number varchar(64), 
     email varchar(256), 
     birthday varchar(256),
-    FOREIGN KEY contacts(userID) REFERENCES users(userID) ON DELETE CASCADE
+    FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE
 )
 
 -- update users table with SIGN-UP
@@ -28,18 +28,28 @@ INSERT INTO users VALUES(
 -- check users for login_name and password
 -- if none returned, not a user
 SELECT userID FROM users
-WHERE {login_data} = login_name AND {password} = password
+WHERE login_name = {arg_login_name} AND password = {arg_password}
+
+-- delete account (users)
+DELETE FROM users
+WHERE login_name = {arg_login_name} AND password = {arg_password}
+
 
 -- create new contact - default autoincrements, userID is from frontend, pass null for values not used
 INSERT INTO contacts VALUES(
-    DEFAULT, {userID}, {name}, {address}, {phonenumber}, {email}, {birthday}
+    DEFAULT, {arg_userID}, {arg_name}, {arg_address}, {arg_phone_number}, {arg_email}, {arg_birthday}
 )
-
--- update contact - pass in all old data, but replace new data 
-UPDATE contacts
-SET name = {new}, address = {new}, phonenumber = {new}, email = {new}, birthday = {new}
-WHERE contactID = {contactID}
 
 -- get all contacts
 SELECT * FROM contacts
-WHERE userID = {userID}
+WHERE userID = {arg_userID}
+
+-- update contact - pass in all old data, but replace new data 
+UPDATE contacts
+SET name = {arg_new}, address = {arg_new}, phone_number = {arg_new}, email = {arg_new}, birthday = {arg_new}
+WHERE contactID = {arg_contactID}
+
+-- delete contacts - based on contact ID
+DELETE FROM contacts
+WHERE contactID = {arg_contactID}
+
