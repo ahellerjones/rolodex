ae283116e301f4ae77d15369cc12d7c1a7a8cf7d
ahellerjones <hellerar@vt.edu>
2023-06-01 22:46:14 -0400

    return the proper userId

--- slick_backend/main.go
+++ slick_backend/main.go
@@ -5,20 +5,19 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
-	"json"
+	"encoding/json"
 )
 
 func main() {
 	// Define the HTTP endpoints and their corresponding handler functions
 	http.HandleFunc("/", landingHandler)
 	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/user", userHandler)
+	http.HandleFunc("/contacts", contactsHandler)
 
 	// Start the server on port 8080
 	log.Println("Server listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
 
 func landingHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../sexy_frontend/dist/index.html")
@@ -47,7 +46,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		// If we're just trying to login
 		if loginInfo.loginOrSetup { 
-			err = checkUsernamePassword(loginInfo)
+			err = checkUsernamePassword(loginInfo) // TODO 
 			if err != nil {
 				http.Error(w, err.Error(), 500)
 				return
@@ -56,7 +55,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "{%s}", loginInfo.user)
 		} else { 
 			// Else we're trying to setup an account 
-			err = checkIfUserExists(loginInfo)
+			err = checkIfUserExists(loginInfo) // TODO 
 			if err != nil { 
 				http.Error(w, err.Error(), 500)
 				return
@@ -73,17 +72,51 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	// Set the response content type
-	w.Header().Set("Content-Type", "application/json")
+func contactsHandler(w http.ResponseWriter, r *http.Request) {
+switch r.Method {
+	case http.MethodGet:
+		getContacts	
 
-	// Simulate fetching user data from a database
-	user := struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-	}{
-		Name:  "John Doe",
-		Email: "johndoe@example.com",
+
+
+
+	case http.MethodPost:
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
 	}
-
+	defer r.Body.Close()
+	var loginInfo LoginInfo
+	err = json.Unmarshal(body, &loginInfo)
+	// This should catch all shitty unmarshalling
+	if err != nil {
+		http.Error(w, err.Error(), 410)
+		return
+	}
+	// If we're just trying to login
+	if loginInfo.loginOrSetup { 
+		err = checkUsernamePassword(loginInfo) // TODO 
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.WriteHeader(http.StatusOK) // 200
+		fmt.Fprint(w, "{%s}", loginInfo.user)
+	} else { 
+		// Else we're trying to setup an account 
+		err = checkIfUserExists(loginInfo) // TODO 
+		if err != nil { 
+			http.Error(w, err.Error(), 500)
+			return
+		} 
+		err = createUser(loginInfo)
+		if err != nil { 
+			http.Error(w, err.Error(), 500)
+			return
+		} 
+	}
+	default:
+		w.WriteHeader(http.StatusConflict) // 410
+		fmt.Fprint(w, "Illegal request to /login")
 }
