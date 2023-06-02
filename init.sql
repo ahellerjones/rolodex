1bd609407a8654617426f3a76e130fb7a2a96501
garcgabe <garciagabe0033@yahoo.com>
2023-06-01 21:53:27 -0400

    added users changes

--- init.sql
+++ init.sql
@@ -1,40 +1,37 @@
 -- Table schema creation
 -- Dim Users table
 
--- userID (PK)
--- firstname
--- lastname
--- login_name
--- password
--- email
-
 CREATE TABLE users(
-    userID int primary key,
-    firstname varchar(256),
-    lastname varchar(256), 
+    userID int AUTO_INCREMENT primary key,
     login_name varchar(64) unique not null,
-    password varchar(256) not null,
-    email varchar(256) unique not null
-)
-
+    password varchar(256) not null
+    )
 -- Mod Contacts table
-
--- contactID (PK)
--- userID (FK, on delete cascade)
--- firstname
--- lastname
--- address
--- phone number
--- email
--- birthday
 
 CREATE TABLE contacts(
     contactID int primary key,
-    userID int foreign key ON DELETE cascade,
+    userID int, 
     firstname varchar(256),
     lastname varchar(256),
     address varchar(256),
-    phonenumber int, 
+    phonenumber varchar(64), 
     email varchar(256), 
-    birthday varchar(256)
+    birthday varchar(256),
+    FOREIGN KEY contacts(userID) REFERENCES users(userID) ON DELETE CASCADE
+)
+
+-- update users table with SIGN-UP
+-- should return success
+INSERT INTO users VALUES(
+    DEFAULT, login_name, password
+)
+
+-- check users for login_name and password
+-- if none returned, not a user
+SELECT userID FROM users
+WHERE {login_data} = login_name AND {password} = password
+
+-- create new contact
+INSERT INTO contacts VALUES(
+    
 )
